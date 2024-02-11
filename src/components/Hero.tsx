a9b334d3b7824e7e57787466f3d2b86e43f3dd68
Mursaleen00 <playessport@gmail.com>
2024-02-11 19:30:08 +0500

    fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 function Hero() {
   return (
-    <div className="font-bold text-xl text-white z-10 mt-20 flex flex-col gap-y-20 items-center justify-center">
+    <div className="font-bold text-xl text-white z-10 h-[80vh] flex flex-col gap-y-20 items-center justify-center">
       <div className="flex w-full justify-center items-center gap-x-3">
         <hr className="w-1/5 md:w-1/4 h-1 bg-white" />
         <div className=" font-semibold md:font-bold text-xs md:text-sm tracking-wider">
