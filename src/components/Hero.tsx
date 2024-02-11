ca60d070d1df0ee7d4ab157aa3ec363a6f71871f
Mursaleen00 <playessport@gmail.com>
2024-02-11 18:41:15 +0500

    opacity set

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hero() {
+  return <div className="font-bold text-xl text-white z-10">Hero</div>;
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-hero bg-cover">
-      <div className="bg-black h-screen opacity-40">
-        <Navbar />
-      </div>
+    <div className="bg-hero bg-cover h-screen relative top-0 -z-10">
+      <div className="bg-black absolute opacity-40 w-full h-screen -z-10" />
+      <Navbar />
+      <Hero />
     </div>
   );
 }
