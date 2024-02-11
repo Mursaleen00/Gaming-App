49e9bc4c31a9d9d4ef5b1b14dd23d92f65ce4f09
Mursaleen00 <playessport@gmail.com>
2024-02-11 18:20:41 +0500

    navbar completed with responsive

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="h-20 flex items-center justify-around">
+      <div className="flex gap-x-2 text-lg items-center">
+        <Image alt="Logo" src={"/icon/logo.svg"} width={32} height={32} />
+        <h1 className="text-white font-pacifico">Medley</h1>
+      </div>
+      <div className="hidden md:flex gap-x-10 items-center">
+        <ul className="flex gap-x-10 text-white font-normal text-sm font-pacifico">
+          <li className="text-theme-second-color cursor-pointer">Home</li>
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Steps</li>
+          <li className="cursor-pointer">Testimonial</li>
+        </ul>
+        <input
+          type="search"
+          className="rounded-full outline-none text-black px-3"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
